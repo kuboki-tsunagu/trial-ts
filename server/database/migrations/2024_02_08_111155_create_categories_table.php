5c6b377de71945bedf3aeb46799458ca53006299
Kuboki Naoto <kuboki.n@tsunagu-grp.jp>
2024-02-08 11:28:53 +0900

    （修正）Categoryマイグレーションファイル

--- server/database/migrations/2024_02_08_111155_create_categories_table.php
+++ server/database/migrations/2024_02_08_111155_create_categories_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
 
         Schema::create('secondary_categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->foreignId('primary_category_id')->constrained();
             $table->timestamps();
         });
